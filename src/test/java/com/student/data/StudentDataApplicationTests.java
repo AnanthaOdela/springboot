8cbf9d2aaa18b5bb3a6472f4b327f0f57d910161
ananth <ananth.cvr@gmail.com>
2024-01-18 00:33:07 +0530

    code refactoring

--- src/test/java/com/student/data/StudentDataApplicationTests.java
+++ src/test/java/com/student/data/StudentDataApplicationTests.java
@@ -3,10 +3,10 @@ package com.student.data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudentDataApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
